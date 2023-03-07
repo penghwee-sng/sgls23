e3cbd953dae0c4ef132c3ebefab7a31686377cb8
Aeora <47289732+Aeoraa@users.noreply.github.com>
2023-03-07 10:31:06 +0800

    refactor: rename zone with dash convention

--- scripts/inventory-generator/inventory-generation.py
+++ scripts/inventory-generator/inventory-generation.py
@@ -12,6 +12,7 @@ linux_server = ""
 linux_client = ""
 for system in data:
     # print(system['os'])
+    system['zones'][0] = system['zones'][0].replace("_","-")
     if "windows_server" in system['os']:
         windows_server = windows_server + f"{system['zones'][0]}_{system['hostname_common']} ansible_host={system['network_interfaces'][0]['addresses'][0]['address_without_subnet']} # {system['network_interfaces'][0]['network_id']} OS:{system['os']}\n"
     elif "windows" in system['os']:
