263830d9157020693c33cf943e5ce8d64f70d3d0
Aeora <47289732+Aeoraa@users.noreply.github.com>
2023-03-07 11:37:16 +0800

    refactor: update variable names

--- scripts/inventory-generator/inventory-generation.py
+++ scripts/inventory-generator/inventory-generation.py
@@ -2,7 +2,7 @@ import json
 
 data = {}
 # Opening JSON file
-with open('dump2.json') as json_file:
+with open(r'resources.json') as json_file:
     data = json.load(json_file)['data']['systems']
  
 # Print the type of data variable
@@ -14,12 +14,12 @@ for system in data:
     # print(system['os'])
     system['zones'][0] = system['zones'][0].replace("_","-")
     if "windows_server" in system['os']:
-        windows_server = windows_server + f"{system['zones'][0]}_{system['hostname_common']} ansible_host={system['network_interfaces'][0]['addresses'][0]['address_without_subnet']} # {system['network_interfaces'][0]['network_id']} OS:{system['os']}\n"
+        windows_server = windows_server + f"{system['zones'][0]}_{system['hostname_common']} ansible_host={system['network_interfaces'][0]['addresses'][0]['address_without_subnet']} os={system['os']} fqdn={system['network_interfaces'][0]['fqdn']} # {system['network_interfaces'][0]['network_id']}\n"
     elif "windows" in system['os']:
-        windows_client = windows_client + f"{system['zones'][0]}_{system['hostname_common']} ansible_host={system['network_interfaces'][0]['addresses'][0]['address_without_subnet']} # {system['network_interfaces'][0]['network_id']} OS:{system['os']}\n"
+        windows_client = windows_client + f"{system['zones'][0]}_{system['hostname_common']} ansible_host={system['network_interfaces'][0]['addresses'][0]['address_without_subnet']} os={system['os']} fqdn={system['network_interfaces'][0]['fqdn']} # {system['network_interfaces'][0]['network_id']}\n"
     elif "desktop" in system['os']:
-        linux_client = linux_client + f"{system['zones'][0]}_{system['hostname_common']} ansible_host={system['network_interfaces'][0]['addresses'][0]['address_without_subnet']} # {system['network_interfaces'][0]['network_id']} OS:{system['os']}\n"
+        linux_client = linux_client + f"{system['zones'][0]}_{system['hostname_common']} ansible_host={system['network_interfaces'][0]['addresses'][0]['address_without_subnet']} os={system['os']} fqdn={system['network_interfaces'][0]['fqdn']} # {system['network_interfaces'][0]['network_id']}\n"
     else:
-        linux_server = linux_server + f"{system['zones'][0]}_{system['hostname_common']} ansible_host={system['network_interfaces'][0]['addresses'][0]['address_without_subnet']} # {system['network_interfaces'][0]['network_id']} OS:{system['os']}\n"
+        linux_server = linux_server + f"{system['zones'][0]}_{system['hostname_common']} ansible_host={system['network_interfaces'][0]['addresses'][0]['address_without_subnet']} os={system['os']} fqdn={system['network_interfaces'][0]['fqdn']} # {system['network_interfaces'][0]['network_id']}\n"
 
 print("[windows_server]\n" + windows_server + "[windows_client]\n" + windows_client + "[linux_server]\n" + linux_server + "[linux_client]\n" + linux_client)
